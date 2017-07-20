b9f495f32d181eab7745d60fb8d925cb2c515ba0
Zita Orbán <zita@potatolondon.com>
2017-07-20 12:33:27 +0100

    setting up new cv files and mavo

---
+++ assets/mavo/mavo-tinymce.js
@@ -0,0 +1,48 @@
+(function($) {
+
+var parser, serializer;
+
+Mavo.Plugins.register("tinymce", {
+	ready: $.include(self.tinymce, "https://cdn.tinymce.com/4/tinymce.min.js").then(() => {
+		parser = new tinymce.html.DomParser();
+		serializer = new tinymce.html.Serializer();
+	})
+});
+
+Mavo.Elements.register(".tinymce", {
+	hasChildren: true,
+	default: true,
+	edit: function() {
+		this.preEdit.then(evt => {
+			if (this.tinymce) {
+				// Previously edited, we already have an editor
+				tinymce.EditorManager.execCommand("mceAddEditor", true, this.tinymce.id);
+				return;
+			}
+
+			// Init for the first time
+			tinymce.init({
+				target: this.element,
+				inline: true,
+				menubar: false,
+				toolbar: "styleselect | bold italic | image link | table | bullist numlist",
+				plugins: "image code link table lists media tabfocus"
+			}).then(editors => {
+				this.tinymce = editors[0];
+
+				this.tinymce.on("change", evt => {
+					this.value = this.getValue();
+				});
+			});
+		});
+	},
+	done: function() {
+		if (this.tinymce) {
+			tinymce.EditorManager.execCommand("mceRemoveEditor", true, this.tinymce.id);
+		}
+	},
+	getValue: element => element.innerHTML,
+	setValue: (element, value) => element.innerHTML = serializer.serialize(parser.parse(value))
+});
+
+})(Bliss);
