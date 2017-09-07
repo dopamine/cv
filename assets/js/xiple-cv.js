45eff775ab8618f739d92a3448d599af10f206f6
Zita Orbán <zita@potatolondon.com>
2017-09-07 17:38:36 +0100

    Detailed show/hide interaction. TODO still: - show/hide animations - better show/hide CTA

--- assets/js/xiple-cv.js
+++ assets/js/xiple-cv.js
@@ -1,9 +1,16 @@
+// $(document).ready(function() {
+//   $("#experience").on("click", "a.job-expand", function() {
+//     $(this).prev().toggleClass("job-detail-show");
+//     $(this).css({
+//       "display": "none"
+//     });
+//     return false;
+//   });
+// });
+
 $(document).ready(function() {
-  $("#experience").on("click", "a.job-expand", function() {
-    $(this).prev().toggleClass("job-detail-show");
-    $(this).css({
-      "display": "none"
-    });
-    return false;
+  $("#experience").on("click", ".job-item", function() {
+    console.log($(this).text());
+    $(this).toggleClass('job-item-verbose');
   });
 });
