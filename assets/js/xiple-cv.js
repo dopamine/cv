40ffad10ed07a7d20d01fe81f46c5b6a28fd2aa0
Zita Orban <zita@potatolondon.com>
2018-05-12 10:33:31 +0100

    Adding first draft of top navigation Re-committing local changes that got lost in the merge chaos Adjusting link colour for increased contrast

--- assets/js/xiple-cv.js
+++ assets/js/xiple-cv.js
@@ -14,3 +14,43 @@ $(document).ready(function() {
     $(this).toggleClass('job-item-verbose');
   });
 });
+
+var prevScroll = 0;
+var navhidden = true;
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+
+    if (scroll < 300 && navhidden == false) {
+      $('.sticky').addClass('hide-sticky');
+      navhidden = true;
+//      console.log('hides 300');
+    } else if (
+           scroll >= 300
+        && scroll < 800
+        && navhidden == true
+        && (scroll - prevScroll) >= 0
+      ) {
+        $('.sticky').removeClass('hide-sticky');
+        navhidden = false;
+//        console.log('appears 300-800');
+    } else if (
+         scroll >= 800
+      && navhidden == false
+      && (scroll - prevScroll) >= 0
+    ) {
+      $('.sticky').addClass('hide-sticky');
+      navhidden = true;
+//      console.log('hides 800');
+    } else if (
+           scroll >= 300
+        && navhidden == true
+        && (scroll - prevScroll) <= -5
+      ) {
+        $('.sticky').removeClass('hide-sticky');
+        navhidden = false;
+//        console.log('appears backwards');
+    }
+
+     prevScroll = scroll;
+});
